bed8eddef3774609093ee559507e5a3b76e9cea4
Victor Scherbackov <victor@90victor09.ru>
2024-03-17 09:27:32 +0300

    Import skeleton

---
+++ tests/test_provider_octodns_yandex.py
@@ -0,0 +1,14 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_yandex import YandexCloudProvider
+
+
+class TestYandexCloudProvider(TestCase):
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        YandexCloudProvider
