ec3034afb3c26d7bbf48ad7a08663b7cec90ef34
Victor Scherbackov <victor@90victor09.ru>
2024-03-25 18:59:33 +0300

    Add CM & CDN sources

--- octodns_yandex/__init__.py
+++ octodns_yandex/__init__.py
@@ -4,9 +4,17 @@
 from .record import YandexCloudAnameRecord
 from .version import __VERSION__, __version__
 from .yandex360_provider import Yandex360Provider
+from .yandexcloud_cdn_source import YandexCloudCDNSource
+from .yandexcloud_cm_source import YandexCloudCMSource
 from .yandexcloud_provider import YandexCloudProvider
 
-__all__ = ['YandexCloudProvider', 'Yandex360Provider', 'YandexCloudAnameRecord']
+__all__ = [
+    'YandexCloudProvider',
+    'Yandex360Provider',
+    'YandexCloudAnameRecord',
+    'YandexCloudCMSource',
+    'YandexCloudCDNSource',
+]
 
 # quell warnings
 __VERSION__
--- octodns_yandex/yandexcloud_provider.py
+++ octodns_yandex/yandexcloud_provider.py
@@ -1,5 +1,3 @@
-import json
-import subprocess
 from logging import getLogger
 
 import grpc
@@ -16,20 +14,13 @@ from yandex.cloud.dns.v1.dns_zone_service_pb2_grpc import DnsZoneServiceStub
 
 from octodns import __VERSION__ as octodns_version
 from octodns.idna import idna_decode
-from octodns.provider import ProviderException
 from octodns.provider.base import BaseProvider
 from octodns.record import Record
 
+from octodns_yandex.auth import _AuthMixin
+from octodns_yandex.exception import YandexCloudException
 from octodns_yandex.record import YandexCloudAnameRecord
 from octodns_yandex.version import __VERSION__ as provider_version
-
-
-class YandexCloudException(ProviderException):
-    pass
-
-
-class YandexCloudConfigException(YandexCloudException):
-    pass
 
 
 def _aname_type_map(rset):
@@ -90,14 +81,7 @@ def map_octodns_to_rset(record: Record):
     )
 
 
-AUTH_TYPE_OAUTH = 'oauth'
-AUTH_TYPE_IAM = 'iam'
-AUTH_TYPE_METADATA = 'metadata'
-AUTH_TYPE_SA_KEY = 'sa-key'
-AUTH_TYPE_YC_CLI = 'yc-cli'
-
-
-class YandexCloudProvider(BaseProvider):
+class YandexCloudProvider(_AuthMixin, BaseProvider):
     SUPPORTS_GEO = False
     SUPPORTS_DYNAMIC = False
     SUPPORTS_MULTIVALUE_PTR = True
@@ -166,53 +150,6 @@ class YandexCloudProvider(BaseProvider):
             **self.auth_kwargs,
         )
         self.dns_service = self.sdk.client(DnsZoneServiceStub)
-
-    @staticmethod
-    def get_auth_kwargs(auth_type, oauth_token, iam_token, sa_key_file, sa_key):
-        if auth_type == AUTH_TYPE_OAUTH:
-            return {'token': oauth_token}
-        elif auth_type == AUTH_TYPE_IAM:
-            return {'iam_token': iam_token}
-        elif auth_type == AUTH_TYPE_METADATA:
-            return {}  # Auto configured by SDK
-        elif auth_type == AUTH_TYPE_SA_KEY:
-            if sa_key_file:
-                try:
-                    with open(sa_key_file) as infile:
-                        sa_key = json.load(infile)
-                except OSError as e:
-                    raise YandexCloudConfigException(
-                        f"Failed to open 'sa_key_file': {sa_key_file}"
-                    ) from e
-            if (
-                sa_key is None
-                or 'id' not in sa_key
-                or 'service_account_id' not in sa_key
-                or 'private_key' not in sa_key
-            ):
-                raise YandexCloudConfigException(
-                    "Provider option 'sa_key' should be dict with fields: id, service_account_id, private_key"
-                )
-            return {'service_account_key': sa_key}
-        elif auth_type == AUTH_TYPE_YC_CLI:
-            process = None
-            try:
-                process = subprocess.run(
-                    ['yc', 'config', 'get', 'token'], stdout=subprocess.PIPE
-                )
-                process.check_returncode()
-            except FileNotFoundError as e:
-                raise YandexCloudConfigException(
-                    'yc binary not found in PATH'
-                ) from e
-            except subprocess.CalledProcessError as e:
-                raise YandexCloudConfigException(
-                    f"Failed to get token from yc, exit code: {process.returncode}"
-                ) from e
-
-            return {'token': process.stdout.decode('utf-8').strip()}
-        else:
-            raise YandexCloudConfigException("Unknown auth type")
 
     def get_zone_id_by_name(self, zone_name):
         decoded_name = idna_decode(zone_name)
