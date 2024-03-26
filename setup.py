d63a923fa5357da99b45220f4dd1694d4fc6d60b
Victor Scherbackov <victor@90victor09.ru>
2024-03-26 08:36:36 +0300

    Fix setuptools in 3.12

--- setup.py
+++ setup.py
@@ -42,7 +42,12 @@ setup(
         ),
         'test': tests_require,
     },
-    install_requires=('octodns>=1.0.0', 'yandexcloud==0.265.0'),
+    install_requires=(
+        'octodns>=1.0.0',
+        'yandexcloud>=0.267.0',
+        # used in yandexcloud, not installed in venv since 3.12
+        'setuptools',
+    ),
     license='MIT',
     long_description=long_description,
     long_description_content_type='text/markdown',
